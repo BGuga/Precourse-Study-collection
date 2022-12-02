761ad75469c7c7446b4b21c6e8b92a7d6a352e8b
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-02 21:17:01 +0900

    feat: 역 관리 기능의 skeleteon code작성

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -6,4 +6,20 @@ import subway.view.StationOutputView;
 public class StationController {
     StationInputView inputView = new StationInputView();
     StationOutputView outputView = new StationOutputView();
+
+    public void process() {
+        processByCommand(inputView.getCommandByConsole());
+    }
+
+    private void processByCommand(StationCommand command) {
+        if (command == StationCommand.ENROLL) {
+            enrollStation()
+        }
+        if (command == StationCommand.DELETE) {
+            deleteStation();
+        }
+        if (command == StationCommand.SEARCH) {
+            serachStation();
+        }
+    }
 }
