f023e5e69e80f3ad4077c4ae98be412ec4de2c61
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-02 20:58:49 +0900

    docs(README): 역 관리 기능의 입,출력 기능 목록 작성

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,5 +1,9 @@
 package subway.controller;
 
+import subway.view.StationInputView;
+import subway.view.StationOutputView;
+
 public class StationController {
-    
+    StationInputView inputView = new StationInputView();
+    StationOutputView outputView = new StationOutputView();
 }
