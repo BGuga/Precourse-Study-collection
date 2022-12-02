107ab1b050c6930832961e1943d83a1cbefb684c
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-02 16:42:13 +0900

    feat: 역을 등록하는 기능 구현

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,11 +1,13 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class StationRepository {
+    static {
+
+    }
+
+    public static final String ALREADY_EXITING_STATION_ERROR_MESSAGE = "[ERROR] 이미 등록된 역입니다.";
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
@@ -13,10 +15,21 @@ public class StationRepository {
     }
 
     public static void addStation(Station station) {
+        stationDuplicationCheck(station);
         stations.add(station);
     }
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    private static void stationDuplicationCheck(Station station) {
+        if (isExistingStation(station)) {
+            throw new IllegalArgumentException(ALREADY_EXITING_STATION_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isExistingStation(Station station) {
+        return stations.stream().anyMatch(st -> Objects.equals(st.getName(), station.getName()));
     }
 }
