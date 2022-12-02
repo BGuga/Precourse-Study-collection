d9fea50aaa84eaef5546dbf5399f12ec7e17f122
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-02 16:18:03 +0900

    feat: 노선에 역을 추가하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,13 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+    public static final String ALREADY_EXITING_STATION_ERROR_MESSAGE = "[ERROR] 이미 노선에 존재하는 역입니다";
+
     private String name;
+    private List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -9,6 +15,26 @@ public class Line {
 
     public String getName() {
         return name;
+    }
+
+    public void addStation(Station station, int index) {
+        stationValidation(station);
+    }
+
+    private void stationValidation(Station station) {
+        checkDuplications(station);
+    }
+
+    private void checkDuplications(Station station) {
+        for (Station originStation : stations) {
+            checkDuplication(originStation, station);
+        }
+    }
+
+    private void checkDuplication(Station originStation, Station compareStation) {
+        if (originStation.getName().equals(compareStation)) {
+            throw new IllegalArgumentException(ALREADY_EXITING_STATION_ERROR_MESSAGE);
+        }
     }
 
     // 추가 기능 구현
