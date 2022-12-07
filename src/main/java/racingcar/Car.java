ca32f45e0a3785645724f3b55f488b48f4de44a6
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-07 16:49:02 +0900

    feat: 자동차는 이름과 위치를 갖는다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,25 @@
 package racingcar;
 
 public class Car {
+    private final static int MIN_CAR_NAME_LENGTH = 1;
+    private final static int MAX_CAR_NAME_LENGTH = 5;
+    private final static String INVALID_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 유효하지 않은 자동차의 이름 길이입니다.";
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        nameValidation(name);
         this.name = name;
+    }
+
+    private void nameValidation(String name) {
+        checkNameLength(name.length());
+    }
+
+    private void checkNameLength(int length) {
+        if (length < MIN_CAR_NAME_LENGTH || length > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH_ERROR_MESSAGE);
+        }
     }
 
     // 추가 기능 구현
