ab0967795fd094886b22ab1a2f29e5395d1f6405
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-07 16:53:33 +0900

    feat: 자동차를 이동하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,13 +3,21 @@ package racingcar.domain;
 public class Car {
     private final static int MIN_CAR_NAME_LENGTH = 1;
     private final static int MAX_CAR_NAME_LENGTH = 5;
+    private final static int MIN_CAR_MOVE_INPUT = 0;
+    public static final int MAX_CAR_MOVE_INPUT = 9;
     private final static String INVALID_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 유효하지 않은 자동차의 이름 길이입니다.";
+    public static final String INVALID_MOVE_INPUT_MESSAGE = "[ERROR] 유효하지 않은 이동값의 입력입니다.";
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         nameValidation(name);
         this.name = name;
+    }
+
+    public void move(int num) {
+        validateMoveNum(num);
+        this.position++;
     }
 
     private void nameValidation(String name) {
@@ -19,6 +27,12 @@ public class Car {
     private void checkNameLength(int length) {
         if (length < MIN_CAR_NAME_LENGTH || length > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateMoveNum(int num) {
+        if (num < MIN_CAR_NAME_LENGTH || num > MAX_CAR_MOVE_INPUT) {
+            throw new IllegalArgumentException(INVALID_MOVE_INPUT_MESSAGE);
         }
     }
 
