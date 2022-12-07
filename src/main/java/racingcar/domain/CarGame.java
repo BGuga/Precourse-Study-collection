f99c3ba316510017d4764fd229d0d7f3ff4bda5d
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-07 17:24:14 +0900

    feat: 우승 자동차들을 반환하는 기능 구현

--- src/main/java/racingcar/domain/CarGame.java
+++ src/main/java/racingcar/domain/CarGame.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class CarGame {
     private final List<Car> enrolledCars;
@@ -21,6 +22,13 @@ public class CarGame {
         return multiPositions;
     }
 
+    public List<Car> getWinningCars() {
+        int winningPosition = getWinningCarPosition();
+        return enrolledCars.stream()
+                .filter(car -> car.getPosition().getPosition() == winningPosition)
+                .collect(Collectors.toList());
+    }
+
     private MultiPosition moveCars() {
         List<Position> positions = new ArrayList<>();
         for (Car car : enrolledCars) {
@@ -33,5 +41,16 @@ public class CarGame {
     private CarMoveValue makeRandomCarMoveValue() {
         int randomIntValue = Randoms.pickNumberInRange(CarMoveValue.MIN_CAR_MOVE_INPUT, CarMoveValue.MAX_CAR_MOVE_INPUT);
         return new CarMoveValue(randomIntValue);
+    }
+
+    private int getWinningCarPosition() {
+        int maxPosition = enrolledCars.get(0).getPosition().getPosition();
+        for (Car car : enrolledCars) {
+            int carPosition = car.getPosition().getPosition();
+            if (maxPosition < carPosition) {
+                maxPosition = carPosition;
+            }
+        }
+        return maxPosition;
     }
 }
