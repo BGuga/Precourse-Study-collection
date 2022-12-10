2ac1167304c1b45aef5a0cfb5ddfbca3e67e5c50
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-10 13:57:48 +0900

    feat: 돈은 10으로 나누어 떨어지는 정수이다

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,2 +1,18 @@
-package vendingmachine.domain;public class Money {
+package vendingmachine.domain;
+
+public class Money {
+    public static final String INVALID_MONEY_ERROR_MESSAGE = "[ERROR] 돈은 10으로 나누어 떨어져야 합니다.";
+
+    private final int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money % 10 != 0) {
+            throw new IllegalArgumentException(INVALID_MONEY_ERROR_MESSAGE);
+        }
+    }
 }
