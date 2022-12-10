e3674d5bde43c5974287a4a88936613ab78b7441
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-10 18:17:52 +0900

    feat: 등록된 잔돈 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,4 +1,23 @@
 package vendingmachine.view;
 
+import vendingmachine.domain.Coin;
+
+import java.util.Map;
+
 public class OutputView {
+
+    public void printExchange(Map<Coin, Integer> coins) {
+        System.out.println("자판기가 보유한 동전");
+        printAllCoins(coins);
+    }
+
+    private void printAllCoins(Map<Coin, Integer> coins) {
+        for (Coin coin : Coin.values()) {
+            printCoin(coin, coins.getOrDefault(coin, 0));
+        }
+    }
+
+    private void printCoin(Coin coin, Integer coinCount) {
+        System.out.println(String.format("%d원 - %d개", coin.getAmount(), coinCount));
+    }
 }
