da3fc62c97ad47a41cf851718b574e2ce5a8fff2
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-10 14:29:46 +0900

    feat: 물건의 수량은 0이상이다

--- src/main/java/vendingmachine/domain/ProductAmount.java
+++ src/main/java/vendingmachine/domain/ProductAmount.java
@@ -2,4 +2,18 @@ package vendingmachine.domain;
 
 public class ProductAmount {
 
+    public static final String MIN_AMOUNT_ERROR_MESSAGE = "[ERROR] 물건의 수량은 %d이상 이여야 합니다.";
+    public static final int MIN_AMOUNT = 0;
+    private final int amount;
+
+    public ProductAmount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount < MIN_AMOUNT) {
+            throw new IllegalArgumentException(String.format(MIN_AMOUNT_ERROR_MESSAGE, MIN_AMOUNT));
+        }
+    }
 }
