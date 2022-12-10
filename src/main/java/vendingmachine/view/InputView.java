760321ea97f5f7ed31899fe0fa502d52a329b085
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-10 17:46:23 +0900

    feat: 잔돈을 입력 받는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,4 +1,25 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.Money;
+
 public class InputView {
+    public Money readChangesAmount() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        try {
+            return new Money(getIntValue());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readChangesAmount();
+        }
+    }
+
+    private int getIntValue() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해주세요");
+            return getIntValue();
+        }
+    }
 }
