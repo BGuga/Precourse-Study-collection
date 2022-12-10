626ad62ea0f1bb5de9d0ef2c8bc310edc0c44404
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-10 16:48:21 +0900

    feat: 자판기에서 동전 기계에 돈을 넣는 기능 구현

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -8,10 +8,15 @@ public class VendingMachine {
     public VendingMachine() {
         this.coinMachine = new CoinMachine();
         this.productMachine = new ProductMachine();
+        this.insertedMoney = new Money(0);
     }
 
     public void addProductBundle(ProductBundle productBundle) {
         productMachine.addProducts(productBundle);
+    }
+
+    public void insertMoneyInCoinMachine(Money money) {
+        coinMachine.insertMoney(money);
     }
 
     public void insertMoney(Money money) {
